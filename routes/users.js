2e2eefddb3414ecda63b7ecc16428b319229f534
Rom Stevens <romney.stevens@gmail.com>
2015-06-02 17:48:47 -0500

    Add login/registration pages (FE only)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Harmony | Please Log In' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Harmony | Welcome to Harmony' });
 });
 
 module.exports = router;
