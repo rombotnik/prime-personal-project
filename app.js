e519ab9ceb7523808e2005824027bc2170b06faf
Rom Stevens <romney.stevens@gmail.com>
2015-06-02 16:54:45 -0500

    Add edit page (FE only)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var play = require('./routes/play');
+var edit = require('./routes/edit');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/play', play);
+app.use('/edit', edit);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
